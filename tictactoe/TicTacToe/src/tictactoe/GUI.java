8d6d67c51fcb07e96f79993125b74fa18c3115c0
Christian Remi Wewer <christianwewer@gmail.com>
2018-05-06 15:00:18 +0200

    added some gui componennt

---
+++ tictactoe/TicTacToe/src/tictactoe/GUI.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+
+	private static final long serialVersionUID = 1L; //Is required due to class being serialized. Probably a JFrame requirement.
+	
+	
+	JPanel pane = new JPanel();		
+	
+}
